7a2f4823bd17b47e6256a4c66379affec1aba985
Swarup Ghosh <snwg@live.com>
2020-06-07 17:29:19 +0530

    Added pytest

--- image_augmentation/wide_resnet/wrn_test.py
+++ image_augmentation/wide_resnet/wrn_test.py
@@ -5,18 +5,34 @@ from tensorflow.keras.utils import plot_model
 from image_augmentation.wide_resnet.wrn import WideResNet
 
 
+def visualize_model(model):
+    net_dig_path = '/tmp/model.png'
+    plot_model(net, net_dig_path, show_shapes=True)
+    os.system('open ' + net_dig_path)
+
+
 def test_wrn_28_10():
     inp_shape = (32, 32, 3)
 
     net = WideResNet(inp_shape, depth=28, k=10)
     net.summary()
 
-    net_dig_path = '/tmp/model.png'
-    plot_model(net, net_dig_path, show_shapes=True)
-    os.system('open ' + net_dig_path)
+    visualize_model(net)
+
+    assert True
+
+
+def test_wrn_40_2():
+    inp_shape = (224, 224, 3)
+
+    net = WideResNet(inp_shape, depth=40, k=2)
+    net.summary()
+
+    visualize_model(net)
 
     assert True
 
 
 if __name__ == '__main__':
     test_wrn_28_10()
+    test_wrn_40_2()
