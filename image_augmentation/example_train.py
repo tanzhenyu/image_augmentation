071bf4b6ae984151a03036e4c77c609f3420d0b7
Swarup Ghosh <snwg@live.com>
2020-06-13 13:49:24 +0530

    Minor changes

--- image_augmentation/example_train.py
+++ image_augmentation/example_train.py
@@ -27,11 +27,13 @@ model.summary()
 
 batch_size = 128
 epochs = 200
+restart_steps = 10
+init_learn_rate = 0.01
 
 train_ds = cifar10['train'].cache().shuffle(
         1000, reshuffle_each_iteration=True).batch(batch_size)
 
-lr_schedule = keras.experimental.CosineDecayRestarts(0.01, 10)
+lr_schedule = keras.experimental.CosineDecayRestarts(init_learn_rate, restart_steps)
 opt = keras.optimizers.SGD(lr_schedule, momentum=0.9)
 
 model.compile(opt, loss='sparse_categorical_crossentropy',
