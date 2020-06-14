e92c6864a8d5eec0ba502218f49fc1be0c799ce9
Swarup Ghosh <snwg@live.com>
2020-06-14 17:40:20 +0530

    Added cutout, bug fixes reqd for increasing img shape

--- image_augmentation/image/image_ops.py
+++ image_augmentation/image/image_ops.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
+_gray = (128, )
 
 @tf.function
 def invert(img):
@@ -12,3 +13,35 @@ def invert(img):
     inv_img = tf.cast(inv_img, orig_dtype)
 
     return inv_img
+
+
+@tf.function
+def cutout(img, size=16, color=None):
+    img = tf.convert_to_tensor(img)
+
+    img_shape = tf.shape(img)
+    height, width, channels = img_shape[0], img_shape[1], img_shape[2]
+
+    loc_x = tf.random.uniform((), 0, width, tf.int32)
+    loc_y = tf.random.uniform((), 0, height, tf.int32)
+
+    ly, lx = tf.maximum(0, loc_y - size // 2), tf.maximum(0, loc_x - size // 2)
+    uy, ux = tf.minimum(height, loc_y + size // 2), tf.minimum(width, loc_x + size // 2)
+
+    if color is None:
+        color = tf.convert_to_tensor(_gray * channels, img.dtype)
+    else:
+        color = tf.convert_to_tensor(color, img.dtype)
+
+    cut = tf.ones((uy - ly, ux - lx, channels), img.dtype)
+
+    top = img[0: lx, 0: width]
+    between = tf.concat([
+        img[ly: uy, 0: lx],
+        cut * color,
+        img[ly: uy, ux: width]
+    ], axis=1)
+    bottom = img[ux: height, 0: width]
+
+    cutout_img = tf.concat([top, between, bottom], axis=0)
+    return cutout_img
