c39b15c3cef06fae95305b5948740585e75da4f9
Swarup Ghosh <snwg@live.com>
2020-06-28 23:30:14 +0530

    Minor update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tanzhenyu/image_augmentation",
-    packages=setuptools.find_packages() + requirements,
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
