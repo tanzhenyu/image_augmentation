6eefc7ca599316741bc284559b284d55a98f1e21
Swarup Ghosh <snwg@live.com>
2020-07-02 02:20:59 +0530

    Added transform's class and method to __init__

--- image_augmentation/image/__init__.py
+++ image_augmentation/image/__init__.py
@@ -1,3 +1,5 @@
 from image_augmentation.image.image_ops import cutout, invert, solarize, equalize, blend, posterize, contrast
 from image_augmentation.image.image_ops import auto_contrast, color, sharpness, sample_pairing, brightness
 from image_augmentation.image.layers import RandomCutout
+
+from image_augmentation.image.transform import PolicyAugmentation, some_test_policy, autoaugment_policy
