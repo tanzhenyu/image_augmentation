8e62e98052e7e546ea57357d157bbf8118173563
Swarup Ghosh <snwg@live.com>
2020-07-04 20:02:50 +0530

    Added example for PolicyAugmentation example on policy for reduced ImageNet

---
+++ image_augmentation/augmentation_example.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+from matplotlib import pyplot as plt
+import random
+
+from image_augmentation.image import PolicyAugmentation, autoaugment_policy
+
+image_paths = tf.io.gfile.glob("/Volumes/Card/Datasets/flower_photos/tulips/*.jpg")
+random.shuffle(image_paths)
+
+subset_size = 20
+
+image_paths_ss = tf.convert_to_tensor(image_paths[:subset_size])
+print(image_paths_ss)
+
+desired_size = 331
+
+
+@tf.function
+def read_image(path):
+    image = tf.io.read_file(path)
+    image = tf.image.decode_jpeg(image)
+    image = tf.image.resize_with_crop_or_pad(image, desired_size, desired_size)
+    return image
+
+
+images = tf.map_fn(read_image, image_paths_ss, dtype=tf.uint8)
+policy = autoaugment_policy()
+augmenter = PolicyAugmentation(policy)
+
+augmented_images = augmenter(images)
+
+
+def show_images(images):
+    plt.figure(figsize=[subset_size // 3, subset_size // 3])
+    for idx, image in enumerate(images):
+        plt.subplot(subset_size // 4, 4, idx + 1)
+        plt.imshow(image.numpy())
+        plt.axis("off")
+
+
+show_images(images)
+plt.suptitle("Original Images")
+
+show_images(augmented_images)
+plt.suptitle("Image Data Augmentation using AutoAugment Policies found on reduced ImageNet")
+plt.show()
