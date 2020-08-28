8a5cb8b3fe5b62867af5c9dad6347bf0ab050e06
Swarup Ghosh <snwg@live.com>
2020-08-29 04:33:39 +0530

    Use experimental_steps_per_execution=steps_per_epoch in train_wrn

--- image_augmentation/ai_platform/train_wrn.py
+++ image_augmentation/ai_platform/train_wrn.py
@@ -402,7 +402,8 @@ def main(args):
             logging.info("Using loss: Cross Entropy")
             loss_fn = tf.keras.losses.sparse_categorical_crossentropy
 
-        model.compile(optimizer=opt, loss=loss_fn, metrics=metrics)
+        model.compile(optimizer=opt, loss=loss_fn, metrics=metrics,
+                      experimental_steps_per_execution=steps_per_epoch)
 
     # prepare tensorboard logging
     tb_path = args.job_dir + '/tensorboard'
