e8ad9e116272040c98dbb7bbbe124bf9bfc89240
Swarup Ghosh <snwg@live.com>
2020-09-15 01:06:19 +0530

    Update docs for callbacks

--- image_augmentation/callbacks/extra_eval.py
+++ image_augmentation/callbacks/extra_eval.py
@@ -3,6 +3,20 @@ from tensorflow.keras.callbacks import Callback
 
 
 class ExtraValidation(Callback):
+    """Log evaluation metrics of an extra validation set. This callback
+    is useful for model training scenarios where multiple validation sets
+    are used for evaluation (as Keras by default, provides functionality for
+    evaluating on a single validation set only).
+
+    The evaluation metrics are also logged to TensorBoard.
+
+    Args:
+        validation_data: a tf.data.Dataset pipeline used to evaluate the
+            model, essentially an extra validation dataset.
+        tensorboard_path: path to the TensorBoard logging directory.
+        validation_freq: number of epochs to wait before performing
+            subsequent evaluations.
+    """
     def __init__(self, validation_data, tensorboard_path, validation_freq=1):
         super(ExtraValidation, self).__init__()
 
@@ -16,11 +30,14 @@ class ExtraValidation(Callback):
     def on_epoch_end(self, epoch, logs=None):
         # evaluate at an interval of `validation_freq` epochs
         if (epoch + 1) % self.validation_freq == 0:
+            # gather metric names form model
             metric_names = ['{}_{}'.format('epoch', metric.name)
                             for metric in self.model.metrics]
+            # TODO: fix `model.evaluate` memory leak on TPU
+            # gather the evaluation metrics
+            scores = self.model.evaluate(self.validation_data, verbose=2)
 
-            scores = self.model.evaluate(self.validation_data, verbose=1)
-
+            # gather evaluation metrics to TensorBoard
             with self.tensorboard_writer.as_default():
                 for metric_name, score in zip(metric_names, scores):
                     tf.summary.scalar(metric_name, score, step=epoch)
--- image_augmentation/callbacks/lr_logger.py
+++ image_augmentation/callbacks/lr_logger.py
@@ -3,6 +3,16 @@ from tensorflow.keras.callbacks import Callback
 
 
 class TensorBoardLRLogger(Callback):
+    """Log updates of the optimizer learning rate to TensorBoard during training.
+    Learning rate is logged at the end of each epoch.
+
+    The callback also attempts to log the weight decay applied by the optimizer
+    in case the optimizer makes use of a callable weight decay schedule or a fixed
+    value of weight decay. (eg. SGDW, AdamW)
+
+    Args:
+        writer_path: path to the TensorBoard logging directory.
+    """
     def __init__(self, writer_path):
         super(TensorBoardLRLogger, self).__init__()
         self.writer_path = writer_path
@@ -12,24 +22,31 @@ class TensorBoardLRLogger(Callback):
         self.callable_learning_rate = False
 
     def on_train_begin(self, logs=None):
+        # determine if the model's optimizer uses weight decay
         if hasattr(self.model.optimizer, 'weight_decay'):
             self.use_weight_decay = True
+            # determine if callable weight decay schedule is used
             if hasattr(self.model.optimizer.weight_decay, '__call__'):
                 self.callable_weight_decay = True
+        # determine if callable learning rate schedule is used
         if hasattr(self.model.optimizer.learning_rate, '__call__'):
             self.callable_learning_rate = True
 
     def on_epoch_end(self, batch, logs=None):
         with self.writer.as_default():
+            # callable learning rate schedule
             if self.callable_learning_rate:
                 lr = self.model.optimizer.learning_rate(self.model.optimizer.iterations)
+            # fixed learning rate
             else:
                 lr = self.model.optimizer.learning_rate
             tf.summary.scalar("learning_rate", lr, step=self.model.optimizer.iterations)
 
             if self.use_weight_decay:
+                # callable weight decay schedule
                 if self.callable_weight_decay:
                     wd = self.model.optimizer.weight_decay(self.model.optimizer.iterations)
+                # fixed weight decay
                 else:
                     wd = self.model.optimizer.weight_decay
                 tf.summary.scalar("weight_decay", wd, step=self.model.optimizer.iterations)
