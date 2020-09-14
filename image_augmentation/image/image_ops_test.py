8abc79b870381675ae9362d48fe4e84425718d51
Swarup Ghosh <snwg@live.com>
2020-09-15 02:04:02 +0530

    Updated docs for image_ops_test

--- image_augmentation/image/image_ops_test.py
+++ image_augmentation/image/image_ops_test.py
@@ -8,12 +8,14 @@ from image_augmentation.image.image_ops import sharpness, color, sample_pairing,
 
 
 def _rand_image():
+    """Generates a random uint8 image."""
     image = tf.random.uniform([32, 32, 3], 0, 256, dtype=tf.int32)
     image = tf.cast(image, tf.uint8)
     return image
 
 
 def _display_images(image1, image2):
+    """Display two images using Matplotlib."""
     plt.subplot(1, 2, 1)
     plt.imshow(image1)
 
@@ -24,6 +26,8 @@ def _display_images(image1, image2):
 
 
 def test_invert():
+    """Test the implementation of `invert` with it's corresponding
+    PIL equivalent."""
     image = _rand_image()
     inv_image = invert(image)
 
@@ -40,6 +44,8 @@ def test_invert():
 
 
 def test_solarize():
+    """Test the implementation of `solarize` with it's corresponding
+    PIL equivalent."""
     image = _rand_image()
     threshold = 128
     sol_image = solarize(image, threshold)
@@ -57,6 +63,8 @@ def test_solarize():
 
 
 def test_solarize_add():
+    """Test the implementation of `solarize_ad` with PIL equivalent
+    `PIL.ImageOps.solarize` by using zero add."""
     image = _rand_image()
     addition = 0
     threshold = 128
@@ -75,6 +83,8 @@ def test_solarize_add():
 
 
 def test_cutout():
+    """Test the implementation of `cutout` whether it includes
+    certain grey pixels (replace color) or not."""
     image = _rand_image()
     cut_image = cutout(image)
 
@@ -95,6 +105,8 @@ def test_cutout():
 
 
 def test_posterize():
+    """Test the implementation of `posterize` with it's corresponding
+    PIL equivalent."""
     image = _rand_image()
     bits = 2
     post_image = posterize(image, bits)
@@ -112,6 +124,8 @@ def test_posterize():
 
 
 def test_equalize():
+    """Test the implementation of `equalize` with it's corresponding
+    PIL equivalent."""
     image = tf.random.normal([32, 32, 3], 127, 10)
     image = tf.math.round(image)
     image = tf.cast(image, tf.uint8)
@@ -155,6 +169,8 @@ def test_equalize():
 
 
 def test_auto_contrast():
+    """Test the implementation of `auto_contrast` with it's corresponding
+    PIL equivalent."""
     image = _rand_image()
     ac_image = auto_contrast(image)
 
@@ -170,6 +186,8 @@ def test_auto_contrast():
 
 
 def test_color():
+    """Test the implementation of `color` with it's corresponding
+    PIL equivalent."""
     image = tf.image.decode_jpeg(tf.io.read_file("../../example_images/test_example.jpg"))
     factor = 0.5
     colored_image = color(image, factor)
@@ -190,6 +208,8 @@ def test_color():
 
 
 def test_sharpness():
+    """Test the implementation of `sharpness` with it's corresponding
+    PIL equivalent."""
     image = tf.image.decode_jpeg(tf.io.read_file("../../example_images/test_example.jpg"))
     factor = 0.5
     sharpened_image = sharpness(image, factor)
@@ -210,6 +230,7 @@ def test_sharpness():
 
 
 def test_sample_pairing():
+    """Display two samples fused using SamplePairing."""
     image1 = tf.image.decode_jpeg(tf.io.read_file("../../example_images/tf-flowers_test_example1.jpg"))
     image2 = tf.image.decode_jpeg(tf.io.read_file("../../example_images/tf-flowers_test_example2.jpg"))
 
@@ -243,6 +264,8 @@ def test_sample_pairing():
 
 
 def test_brightness():
+    """Test the implementation of `brightness` with it's corresponding
+    PIL equivalent."""
     image = _rand_image()
     factor = 0.5
     bright_image = brightness(image, factor)
@@ -263,6 +286,8 @@ def test_brightness():
 
 
 def test_contrast():
+    """Test the implementation of `contrast` with it's corresponding
+    PIL equivalent."""
     image = tf.image.decode_jpeg(tf.io.read_file("../../example_images/test_example.jpg"))
     factor = 2.0
     contrast_image = contrast(image, factor)
