3871c9d4640c774192f9aab1256687ed1e803cb2
Swarup Ghosh <snwg@live.com>
2020-09-15 01:30:44 +0530

    Update docs for datasets

--- image_augmentation/datasets/datasets.py
+++ image_augmentation/datasets/datasets.py
@@ -1,4 +1,4 @@
-"""Convenience functions for loading curated dataset(s) as pipeline(s)."""
+"""Convenience functions for loading curated dataset(s) as pipeline(s) using TFDS."""
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
@@ -17,6 +17,13 @@ REDUCED_IMAGENET_VAL_SIZE = 1200
 
 
 def cifar10(data_dir=None):
+    """CIFAR-10 dataset as a `tf.data.Dataset` pipeline.
+
+    Dataset homepage: https://www.cs.toronto.edu/~kriz/cifar.html
+
+    Args:
+        data_dir: directory read/write data from TFDS
+    """
     ds, info = tfds.load('cifar10', read_config=tfds.ReadConfig(skip_prefetch=True),
                          as_supervised=True, with_info=True, data_dir=data_dir)
     return {
@@ -27,6 +34,14 @@ def cifar10(data_dir=None):
 
 
 def svhn(data_dir=None):
+    """Street View Housing Numbers (SVHN) dataset as a
+    `tf.data.Dataset` pipeline.
+
+    Dataset homepage: http://ufldl.stanford.edu/housenumbers/
+
+    Args:
+        data_dir: directory read/write data from TFDS
+    """
     ds, info = tfds.load('svhn_cropped', read_config=tfds.ReadConfig(skip_prefetch=True),
                          as_supervised=True, with_info=True, data_dir=data_dir)
     return {
@@ -37,6 +52,14 @@ def svhn(data_dir=None):
 
 
 def reduced_cifar10(data_dir=None):
+    """Reduced CIFAR-10 dataset as a `tf.data.Dataset` pipeline.
+    The dataset is inspired from work in AutoAugment paper and
+    essentially consists of 4000 randomly selected samples from
+    original CIFAR-10 dataset.
+
+    Args:
+        data_dir: directory read/write data from TFDS
+    """
     ds = tfds.load('cifar10', read_config=tfds.ReadConfig(skip_prefetch=True),
                    as_supervised=True, data_dir=data_dir)
     n_train = tf.data.experimental.cardinality(ds['train'])
@@ -47,11 +70,19 @@ def reduced_cifar10(data_dir=None):
     val_ds = ds.skip(REDUCED_CIFAR_10_TRAIN_SIZE).take(REDUCED_CIFAR_10_VAL_SIZE)
     return {
         "train_ds": train_ds,
-        "val_ds": val_ds
+        "val_ds": val_ds,
+        "test_ds": ds['test']
     }
 
 
 def imagenet(data_dir=None):
+    """Imagenet dataset as `tf.data.Dataset` pipeline.
+    This dataset loads 32 x 32 samples (which had been prepared by
+    applying box resizing method on original high resolution images).
+
+    Dataset homepage: https://patrykchrabaszcz.github.io/Imagenet32/
+
+    """
     ds, info = tfds.load('imagenet_resized/32x32', shuffle_files=True,
                          read_config=tfds.ReadConfig(skip_prefetch=True),
                          as_supervised=True, with_info=True, data_dir=data_dir)
@@ -63,6 +94,14 @@ def imagenet(data_dir=None):
 
 
 def reduced_svhn(data_dir=None):
+    """Reduced SVHN dataset as a `tf.data.Dataset` pipeline.
+    The dataset is inspired from work in AutoAugment paper and
+    essentially consists of 6000 samples from
+    original SVHN dataset.
+
+    Args:
+        data_dir: directory read/write data from TFDS
+    """
     ds = tfds.load('svhn_cropped', read_config=tfds.ReadConfig(skip_prefetch=True),
                    as_supervised=True, data_dir=data_dir)
     n_train = tf.data.experimental.cardinality(ds['train'])
@@ -78,6 +117,14 @@ def reduced_svhn(data_dir=None):
 
 
 def reduced_imagenet(data_dir=None):
+    """Reduced Imagenet (32 x 32) dataset as a `tf.data.Dataset` pipeline.
+    The dataset is inspired from work in AutoAugment paper and
+    essentially consists of 6000 samples selected from 120
+    random Imagenet classes.
+
+    Args:
+        data_dir: directory read/write data from TFDS
+    """
     ds, info = tfds.load("imagenet_resized/32x32", shuffle_files=True,
                          read_config=tfds.ReadConfig(skip_prefetch=True),
                          as_supervised=True, with_info=True, data_dir=data_dir)
@@ -113,6 +160,17 @@ def reduced_imagenet(data_dir=None):
 
 
 def large_imagenet(data_dir=None):
+    """Imagenet dataset as a `tf.data.Dataset` pipeline.
+
+    Note: JPEG decoding is skipped while loading this dataset
+    and hence binary strings representing each image are returned.
+    This is to improve the pipeline performance when additional
+    transformations like random crops are used for training.
+
+    Dataset homepage: http://www.image-net.org/
+
+    Args:
+        data_dir: directory read/write data from TFDS"""
     ds, info = tfds.load('imagenet2012', shuffle_files=True,
                          read_config=tfds.ReadConfig(skip_prefetch=True),
                          decoders={'image': tfds.decode.SkipDecoding()},
